9c8c22719ef477cf2cd75521d51f4d6d0fa1e408
Cara Zellner <carajeanine@gmail.com>
2020-10-10 22:33:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,10 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name_array)
-  name_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   
-  name_array.each do |name|
-    puts "Hello, my name is #{name}."
-  end
   
 end
